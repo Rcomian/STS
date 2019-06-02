867f5f28252516840743fcc1a92daa4af0fd9a6b
Jim Tupper <github@tupper.org.uk>
2019-06-02 17:57:56 +0100

    Fixing build on linux

---
+++ src/sts.cpp
@@ -0,0 +1,30 @@
+#include "sts.hpp"
+
+
+Plugin *pluginInstance;
+
+void init(rack::Plugin *p) 
+{
+	pluginInstance = p;
+		
+	p->addModel(modelSwitch2to1x11);
+	p->addModel(modelOdyssey);
+	p->addModel(modelIlliad);
+	p->addModel(modelPolySEQ16);
+	p->addModel(modelRingModulator);
+	p->addModel(modelWaveFolder);
+	p->addModel(modelVU_Poly);
+	//p->addModel(modelLFOPoly);
+	
+
+	//p->addModel(modelMidiFile);
+	p->addModel(modelMixer8);
+	//p->addModel(modelOdy);
+	//p->addModel(modelOdyssey);
+	//p->addModel(modelSEQEXP);
+	//p->addModel(modelMidiPlayer);
+	
+
+}
+
+
